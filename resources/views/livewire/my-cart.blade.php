d2fc7b647bb088c010bebda69d055c533bff84e2
Alfredo Dela Vega III <ahdv626@gmail.com>
2022-05-22 22:42:35 +0800

    fix: display of total and checkout button when cart is empty

--- resources/views/livewire/my-cart.blade.php
+++ resources/views/livewire/my-cart.blade.php
@@ -32,34 +32,38 @@
                 @endforelse
             </div>
 
-            <h4 class="subtitle-success">
-                Total: PHP {{ Cart::priceTotal() }}
-            </h4>
+            @if( Cart::count() > 0 )
+                <h4 class="subtitle-success">
+                    Total: PHP {{ Cart::priceTotal() }}
+                </h4>
 
-            <div class="form__wrapper">
-                <form method="POST" action="{{ route('customer.cart.store') }}">
-                    @csrf
-                    <div class="form__group">
-                        <label for="pickup_date">Pick-up Date</label>
-                        <input id="pickup_date" type="number" name="pickup_date" value="{{ old('pickup_date') }}" required >
+                <div class="form__wrapper">
+                    <form method="POST" action="{{ route('customer.cart.store') }}">
+                        @csrf
+                        <div class="form__group">
+                            <label for="pickup_date">Pick-up Date</label>
+                            <input id="pickup_date" type="number" name="pickup_date" value="{{ old('pickup_date') }}" required >
 
-                        @error('pickup_date')
-                            <p class="error-message"><strong>{{ $message }}</strong></p>
-                        @enderror
-                    </div>
+                            @error('pickup_date')
+                                <p class="error-message"><strong>{{ $message }}</strong></p>
+                            @enderror
+                        </div>
 
-                    <div class="form__group">
-                        <label for="delivery_date">Delivery Date</label>
-                        <input id="delivery_date" type="number" name="delivery_date" value="{{ old('delivery_date') }}">
+                        <div class="form__group">
+                            <label for="delivery_date">Delivery Date</label>
+                            <input id="delivery_date" type="number" name="delivery_date" value="{{ old('delivery_date') }}">
 
-                        @error('delivery_date')
-                            <p class="error-message"><strong>{{ $message }}</strong></p>
-                        @enderror
-                    </div>
+                            @error('delivery_date')
+                                <p class="error-message"><strong>{{ $message }}</strong></p>
+                            @enderror
+                        </div>
 
-                    <button type="submit" class="btn primary-btn" style="margin-top: 3rem;">Checkout</button>
-                </form>
-            </div>
+                        <button type="submit" class="btn primary-btn" style="margin-top: 3rem;">Checkout</button>
+                    </form>
+                </div>
+            @else
+                <a href="{{ route('customer.booking') }}" class="btn primary-btn" style="margin-bottom: 3rem;">Book A Service Now</a>
+            @endif
         </div>
     </section>
 </div>
