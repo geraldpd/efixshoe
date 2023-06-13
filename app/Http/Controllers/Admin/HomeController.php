c10ea80efa8d0925aab378469afd14f3693c3140
Alfredo Dela Vega III <ahdv626@gmail.com>
2023-06-13 23:10:02 +0800

    feat: add charts in admin homepage

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Models\Booking;
+use App\Models\PaymentDetail;
 use App\Models\PaymentMethod;
 use App\Models\Service;
+use App\Models\User;
 use App\Models\Voucher;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
@@ -33,6 +36,23 @@ class HomeController extends Controller
         $pmethods = PaymentMethod::count();
         $vouchers = Voucher::count();
 
-        return view('admin.home', compact('bookings', 'services', 'pmethods', 'vouchers'));
+        $date = \Carbon\Carbon::today()->subDays(30);
+        $bookingsPerDay = Booking::select(DB::raw("COUNT(*) as count"), DB::raw('DATE_FORMAT(created_at, "%Y-%m-%d") as date_created'))
+            ->where('created_at', '>=', $date)
+            ->groupBy('date_created')
+            ->pluck('count', 'date_created');
+ 
+        $bookings_lbl = $bookingsPerDay->keys();
+        $bookings_val = $bookingsPerDay->values();
+
+        $salesPerDay = PaymentDetail::select(DB::raw("COUNT(*) as count"), DB::raw("SUM((total_cost-discount)/100) as total_sales"), DB::raw('DATE_FORMAT(created_at, "%Y-%m-%d") as date_created'))
+            ->where('created_at', '>=', $date)
+            ->groupBy('date_created')
+            ->pluck('total_sales', 'date_created');
+ 
+        $sales_lbl = $salesPerDay->keys();
+        $sales_val = $salesPerDay->values();
+
+        return view('admin.home', compact('bookings', 'services', 'pmethods', 'vouchers', 'bookings_lbl', 'bookings_val', 'sales_lbl', 'sales_val'));
     }
 }
--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -63,7 +63,127 @@
                 </div>
             </div>
         </div>
+
+        <div class="row">
+            <div class="col-md-6">
+                <div class="card card-success">
+                    <div class="card-header">
+                        <h3 class="card-title">Bookings Last 30 Days</h3>
+                    </div>
+                    <div class="card-body">
+                        <div class="chart">
+                            <canvas id="bookingsLast30Days" height="100px"></canvas>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="card card-success">
+                    <div class="card-header">
+                        <h3 class="card-title">Total Sales Last 30 Days</h3>
+                    </div>
+                    <div class="card-body">
+                        <div class="chart">
+                            <canvas id="salesLast30Days" height="100px"></canvas>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 
 @endsection
+
+@section('js')
+<script type="text/javascript">
+  
+    var bookings_lbl =  {{ Js::from($bookings_lbl) }};
+    var bookings_val =  {{ Js::from($bookings_val) }};
+
+    const bookingsData = {
+      labels: bookings_lbl,
+      datasets: [{
+        label: 'Bookings Last 30 Days',
+        backgroundColor: 'rgb(0,123,255)',
+        borderColor: 'rgb(0,123,255)',
+        data: bookings_val,
+      }]
+    };
+
+    var sales_lbl =  {{ Js::from($sales_lbl) }};
+    var sales_val =  {{ Js::from($sales_val) }};
+
+    const salesData = {
+      labels: sales_lbl,
+      datasets: [{
+        label: 'Total Sales Last 30 Days',
+        backgroundColor: 'rgb(0,123,255)',
+        borderColor: 'rgb(0,123,255)',
+        data: sales_val,
+      }]
+    };
+
+    const bookingsConfig = {
+      type: 'line',
+      data: bookingsData,
+      options: {
+        legend: {
+            display: false
+        },
+        title: {
+            display: false
+        },
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true,
+                    callback: function (value) { if (Number.isInteger(value)) { return value; } },
+                    stepSize: 1
+                }
+            }]
+        }
+      }
+    };
+
+    const salesConfig = {
+      type: 'bar',
+      data: salesData,
+      options: {
+        legend: {
+            display: false
+        },
+        title: {
+            display: false
+        },
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true,
+                    callback: function (value) { if (Number.isInteger(value)) { return 'PHP ' + value; } },
+                    stepSize: 1000
+                }
+            }]
+        },
+        tooltips: {
+            callbacks: {
+                label: function(tooltipItem, data) {
+                    return 'PHP ' + tooltipItem.yLabel;
+                }
+            }
+        }
+      }
+    };
+
+    const bookingsLast30Days = new Chart(
+      document.getElementById('bookingsLast30Days'),
+      bookingsConfig
+    );
+
+    const salesLast30Days = new Chart(
+      document.getElementById('salesLast30Days'),
+      salesConfig
+    );
+
+</script>
+@stop
